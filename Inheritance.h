b6532e9a9ef1961bbff7083b7c9a8ae729e774b4
Raul Zarate <zarater.raul@gmail.com>
2018-07-26 15:46:03 -0700

    Add files via upload

---
+++ Inheritance.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<cstdlib>
+#include<cstring>
+#include<stdio.h>
+
+
+using namespace std;
+
+class base
+{
+	protected:
+		int *marked; // array to mark activities if done or not
+		int *rating;
+		int num;
+		char **activities;
+		
+	public:
+		base();
+		base(const base&);
+		virtual ~base();
+		void display();
+		void addActivity(char act[]);
+		void markActivity(char act[]);
+		void rateActivity(char act[]);
+		void remove(char act[]);
+		void remove_all();
+};
+
+class athletic: public base
+{
+	public:			
+		~athletic();
+};
+
+class SightSeeing: public base
+{
+	public:
+		~SightSeeing();
+};
--- main.cpp
+++ main.cpp
@@ -1,33 +1,98 @@
-#include "inheritence.h"
+#include "node.h"
+
 
 int main()
 {
-	char a_activity_input[100];
-	int num_user;
-	int x = 5;//user will have to init
-	activity_manager** a_act;
-	athletic for_athletes;
-	site for_sightsee;
-    a_act = new activity_manager*[x];
+	char inputAct[20],yesNo[4],temp[20];
+	int numAct,flag=1,flag2=1,activitesPerfomed[3];
+	cout << "Hello! Welcome to the world of activites" << endl;
+	cout << "The following activities are available:\n1)Athletics\n2)Sight Seeing\n";
+	cout << "NOTE: All inputs are case sensitive!" << endl;
+	cout << "Which activity would you like to do? ";	
+	cin.getline(inputAct,20);
+	Node *head = NULL;
 
-	for(int i = 0; i < x; ++i)	//dowhile loop!! ask( )
-	{
-	cout << "please type an activity" << endl;
-	cout << "What would be the activity?" << endl;
-	cin.get(a_activity_input, 100, '\n'); cin.ignore(100, '\n');
-	a_act[i]->set(a_activity_input);
+		if(strcmp("Athletics",inputAct) == 0)
+		{
+			cout << "\nThe following activities are available in Athletics type" << endl;
+			cout << " 1) Wind surfing\n2)Jet sking\n3)Hiking\n4)Kayaking" << endl; 
+			cout << "These activites require a few skills" << endl;
+			cout << "You would have to drive to the location and possess the respective equipment for the activites\n";
+			cout << "Would you like to continue for this activity, enter Yes else No" << endl;
+			cin >> yesNo;
 
-		/*a_act[i]->display();*/
-	cout << "please describe if athletic activity or sight see activity" << endl;
-	cin >> num_user; cin.ignore(100, '\n');
-	if(num_user == 1)
-   	for_athletes.set(a_act, i);	
-	else if(num_user == 2)
-   	for_sightsee.set(a_act, i);	
-	}
+			if(strcmp(yesNo,"Yes")==0)
+			{
+				base *altheticAct = new athletic();
+				cout << "Out of those 4 activites, how many would you like to do?";
+				cin >> numAct;
+				for(int i = 0 ; i < numAct ; i++)
+				{
+					cout << "Enter name of activity " << (i+1) << ": ";
+					char ch;
+					strcpy(temp,"");
+					if(flag)
+					{
+						while(cin.get(ch) && ch != '\n');
+						flag = 0;
+					}
+					cin.getline(temp,20);
+					altheticAct->addActivity(temp);
+				}
 
-	for_athletes.display();
+				addNode(&head,altheticAct); // adding Node to CLL
+				activitesPerfomed[0] = 1;
+				
+				cout << "How many activites would you to like to mark as done? ";
+				cin >> numAct;
+				for(int i = 0 ; i < numAct ; i++)
+				{
+					cout << "Enter name for activity " << (i+1) << ": ";
+					strcpy(temp,"");
+					char ch;
+					if(flag2)
+					{
+						while(cin.get(ch) && ch != '\n');
+						flag2 = 0;
+					}
+					cin.getline(temp,20);
+					altheticAct->markActivity(temp);
+				} 
+			}
+		}
+		else if(strcmp("Sight Seeing",inputAct) == 0)
+		{
+			cout << "\nThe following places are available in Sight Seeing type" << endl;
+			cout << " 1)Mount Rushmore\n2)Mesa Verde\n3)Carlsbad Caverns\n4)Kilauea" << endl; 
+			cout << "These activites require a few things" << endl;
+			cout << "You would have to drive to the location and these places are open are at only specific hours\n";
+			cout << "Would you like to continue for this activity, enter Yes else No" << endl;
+			cin >> yesNo;
 
-	return 1;
+			if(strcmp(yesNo,"Yes")==0)
+			{
+				base *SightSeeingAct = new SightSeeing();
+				addNode(&head,SightSeeingAct); // adding Node to CLL
+				activitesPerfomed[1] = 1;
+
+				cout << "Out of those 5 places, how many would you like to visit?";
+				cin >> numAct;
+				for(int i = 0 ; i < numAct ; i++)
+				{
+					cout << "Enter name of place " << (i+1) << ": ";
+					char ch;
+					int j=0;
+					strcpy(temp,"");
+					if(flag)
+					{
+						while(cin.get(ch) && ch != '\n');
+						flag = 0;
+					}
+					cin.getline(temp,20);
+					SightSeeingAct->addActivity(temp);
+				}
+			}
+		}
+		displayNode(&head);
+		return 0;
 }
-
